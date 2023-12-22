d369207c413b80e671a5344467ffb8720ff6797d
arpajus <arpajus@gmail.com>
2023-12-22 23:55:53 +0100

    seventh excercise done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
+const sumAll = function (number1, number2) {
+let total = 0;
+if (number1 < 0 || number2 < 0) return "ERROR";
+if(typeof number1 !== "number" || typeof number2 !== "number") return "ERROR";
 
+if (number1 < number2) {
+    for (let i = number1; i <= number2; i++) {
+    total = total + i;
+    }
+} else if (number1 > number2) {
+    for (let i = number2; i <= number1; i++) {
+    total = total + i;
+    }
+} else return number1;
+return total;
 };
-
 // Do not edit below this line
 module.exports = sumAll;
